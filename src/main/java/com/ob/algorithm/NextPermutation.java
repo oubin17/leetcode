81147e5f84ff6899f585880be2da51f539d0052c
oub9527 <81718917@qq.com>
2019-04-16 16:45:17 +0800

    更大的排列

---
+++ src/main/java/com/ob/algorithm/NextPermutation.java
@@ -0,0 +1,38 @@
+package com.ob.algorithm;
+
+/**
+ * @Author: oubin
+ * @Date: 2019/4/11 09:01
+ * @Description: 算法需要将给定数字序列重新排列成字典序中下一个更大的排列
+ */
+public class NextPermutation {
+
+    public static void main(String[] args) {
+        int[] x = new int[]{1,3,2};
+        nextPermutation(x);
+    }
+
+    public static void nextPermutation(int[] nums) {
+        int index;
+        int min = nums.length - 1;
+        for (index = nums.length - 1; index > 0 ; index--) {
+            if (nums[index] > 0 && nums[index - 1] < nums[min]) {
+                min = index - 1;
+            }
+            if (nums[index-1] < nums[index]) {
+                int temp = nums[min];
+                nums[min] = nums[index-1];
+                nums[index-1] = temp;
+                for (int j = 0; j < nums.length; j++) {
+                    System.out.println(nums[j]);
+                }
+                break;
+            }
+        }
+        if (index == 0) {
+            for (int j = nums.length - 1; j >= 0; j--) {
+                System.out.println(nums[j]);
+            }
+        }
+    }
+}
