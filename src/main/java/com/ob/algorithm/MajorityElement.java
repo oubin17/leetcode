0df38ffbee69ff2fbd16d761a31d71ae8674bf6d
oub9527 <81718917@qq.com>
2019-05-24 16:19:14 +0800

    找出众数

---
+++ src/main/java/com/ob/algorithm/MajorityElement.java
@@ -0,0 +1,59 @@
+package com.ob.algorithm;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Author: oubin
+ * @Date: 2019/5/24 15:35
+ * @Description: 给定一个大小为 n 的数组，找到其中的众数。众数是指在数组中出现次数大于 ⌊ n/2 ⌋ 的元素。
+ *
+ * 你可以假设数组是非空的，并且给定的数组总是存在众数。
+ * 输入: [2,2,1,1,1,2,2]
+ * 输出: 2
+ *
+ * 从第一个数开始count=1，遇到相同的就加1，遇到不同的就减1，减到0就重新换个数开始计数，总能找到最多的那个
+ */
+public class MajorityElement {
+
+    public static void main(String[] args) {
+        int[] nums = {1};
+        System.out.println(majorityElement(nums));
+    }
+
+    public static int majorityElement(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (null == map.get(nums[i])) {
+                map.put(nums[i], 1);
+                if (map.get(nums[i]) > nums.length/2) {
+                    return nums[i];
+                }
+            } else {
+                map.put(nums[i], map.get(nums[i]) + 1);
+                if (map.get(nums[i]) > nums.length/2) {
+                    return nums[i];
+                }
+            }
+
+        }
+        return 0;
+    }
+
+    public int majorityElement1(int[] nums) {
+        int count = 1;
+        int maj = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (maj == nums[i]) {
+                count++;
+            }
+            else {
+                count--;
+                if (count == 0) {
+                    maj = nums[i + 1];
+                }
+            }
+        }
+        return maj;
+    }
+}
