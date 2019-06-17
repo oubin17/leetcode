3637a608b597f7cdfb1a753fe0c5ac10816ca9a1
oub9527 <81718917@qq.com>
2019-06-17 08:24:00 +0800

    在未排序的数组中找到第 k 个最大的元素。

---
+++ src/main/java/com/ob/algorithm/FindKthLargest.java
@@ -0,0 +1,23 @@
+package com.ob.algorithm;
+
+import java.util.Arrays;
+
+/**
+ * @Author: oubin
+ * @Date: 2019/6/15 15:19
+ * @Description: 在未排序的数组中找到第 k 个最大的元素。请注意，你需要找的是数组排序后的第 k 个最大的元素，而不是第 k 个不同的元素。
+ * 输入: [3,2,3,1,2,4,5,5,6] 和 k = 4
+ * 输出: 4
+ */
+public class FindKthLargest {
+
+    public static void main(String[] args) {
+        int[] nums = {3,2,1,5,6,4};
+        System.out.println(findKthLargest(nums, 2));
+    }
+
+    public static int findKthLargest(int[] nums, int k) {
+        Arrays.sort(nums);
+        return nums[nums.length - k];
+    }
+}
