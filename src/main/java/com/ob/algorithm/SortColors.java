1f213e2162f4b0f0edc1dc01f455f52a0f4b34f2
oub9527 <81718917@qq.com>
2019-06-20 15:49:02 +0800

    荷兰三色旗问题

---
+++ src/main/java/com/ob/algorithm/SortColors.java
@@ -0,0 +1,86 @@
+package com.ob.algorithm;
+
+/**
+ * @Author: oubin
+ * @Date: 2019/6/20 15:11
+ * @Description: 给定一个包含红色、白色和蓝色，一共 n 个元素的数组，原地对它们进行排序，使得相同颜色的元素相邻，并按照红色、白色、蓝色顺序排列。
+ *
+ * 此题中，我们使用整数 0、 1 和 2 分别表示红色、白色和蓝色。
+ *
+ * 注意:
+ * 不能使用代码库中的排序函数来解决这道题。
+ *
+ * 示例:
+ *
+ * 输入: [2,0,2,1,1,0]
+ * 输出: [0,0,1,1,2,2]
+ *进阶：
+ *
+ * 一个直观的解决方案是使用计数排序的两趟扫描算法。
+ * 首先，迭代计算出0、1 和 2 元素的个数，然后按照0、1、2的排序，重写当前数组。
+ * 你能想出一个仅使用常数空间的一趟扫描算法吗？
+ *
+ */
+public class SortColors {
+    public static void main(String[] args) {
+        int[] nums = {2,0,2,1,1,0};
+        sortColors(nums);
+    }
+
+    public static void sortColors(int[] nums) {
+
+        int[] order = new int[nums.length];
+        for (int k = 0; k < nums.length; k++) {
+            order[k] = 1;
+        }
+        int m = 0;
+        int n = nums.length -1;
+        for (int num : nums) {
+            if (num == 0) {
+                order[m++] = 0;
+            }
+            if (num == 2) {
+                order[n--] = 2;
+            }
+        }
+        for (int i = 0; i < order.length; i++) {
+            nums[i] = order[i];
+        }
+
+    }
+
+    /**
+     * 荷兰三色旗问题
+     *
+     * @param nums
+     */
+    public void sortColors2(int[] nums) {
+        // 对于所有 idx < i : nums[idx < i] = 0
+        // j是当前考虑元素的下标
+        int p0 = 0, curr = 0;
+        // 对于所有 idx > k : nums[idx > k] = 2
+        int p2 = nums.length - 1;
+
+        int tmp;
+        while (curr <= p2) {
+            if (nums[curr] == 0) {
+                // 交换第 p0个和第curr个元素
+                // i++，j++
+                tmp = nums[p0];
+                nums[p0++] = nums[curr];
+                nums[curr++] = tmp;
+            }
+            else if (nums[curr] == 2) {
+                // 交换第k个和第curr个元素
+                // p2--
+                tmp = nums[curr];
+                nums[curr] = nums[p2];
+                nums[p2--] = tmp;
+            }
+            else {
+                curr++;
+            }
+        }
+    }
+
+}
