d4fd1ea0492c254601f36ac010405ad2f91e6922
oubin17 <81718917@qq.com>
2020-09-02 00:18:56 +0800

    移除数组中与给定值相同的元素保持原有的元素顺序，并且末尾补0

--- src/main/java/com/ob/algorithm/array/Remove.java
+++ src/main/java/com/ob/algorithm/array/Remove.java
@@ -3,14 +3,15 @@ package com.ob.algorithm.array;
 /**
  * @Author: oubin
  * @Date: 2020/8/31 19:38
- * @Description: 移除数组中与给定值相同的元素[LeetCode]
+ * @Description: 移除数组中与给定值相同的元素保持原有的元素顺序，并且末尾补0
  */
 public class Remove {
 
     public static void main(String[] args) {
         int[] array = new int[]{1, 2, 3, 4, 4, 6, 7};
-        int x = 2;
-        remove(array, x);
+        int x = 4;
+//        remove(array, x);
+        remove2(array, x);
 
         for (int i = 0; i < array.length; i++) {
             System.out.println(array[i]);
@@ -26,5 +27,26 @@ public class Remove {
             }
         }
 
+    }
+
+    /**
+     * 快慢指针的解法，快指针发现不等于的时候，和慢指针对换
+     * @param nums
+     * @param x
+     */
+    public static void remove2(int[] nums, int x) {
+        int left = 0;
+        int right = 0;
+        while (right < nums.length) {
+            if (nums[right] != x) {
+                int temp = nums[right];
+                nums[right] = nums[left];
+                nums[left] = temp;
+                left++;
+            } else {
+                nums[right] = 0;
+            }
+            right++;
+        }
     }
 }
